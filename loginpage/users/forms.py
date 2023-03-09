2489509de414238f09889580ebfb2084bcc6d34b
PamAbaki <annepam4@gmail.com>
2023-03-09 13:28:34 +0300

    added styles

--- loginpage/users/forms.py
+++ loginpage/users/forms.py
@@ -14,19 +14,20 @@ class NewUserForm(UserCreationForm):
         fields = ("username", "password1", "password2")
 
     # Override the help_text of username, password1 and password2 fields
-    username = forms.CharField(help_text='', required=True)
+    username = forms.CharField(label='Username',widget=forms.TextInput(attrs={'placeholder': 'Username'}),help_text='', required=True)
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'placeholder': 'Password'}), help_text='', required=True)
     password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput(attrs={'placeholder': 'Password confirmation'}), help_text='', required=True)
 
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
         if commit:
+            user.set_password(self.cleaned_data['password1'])
             user.save()
             # Create a Details instance for the newly registered user
             details = Details.objects.create(
                 username=user.username,
-                password=user.password,
-                password_confirmation=self.cleaned_data.get('password2')
+                password=self.cleaned_data['password1'],
+                password_confirmation=self.cleaned_data['password2']
             )
         return user
 
